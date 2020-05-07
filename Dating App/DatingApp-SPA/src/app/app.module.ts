8057526077f0184b16c38eb067e0a08d1d418a7e
Bernhardt <Bernhardt.bdt@gmail.com>
2020-05-07 13:44:04 +0200

    new commit

--- Dating App/DatingApp-SPA/src/app/app.module.ts
+++ Dating App/DatingApp-SPA/src/app/app.module.ts
@@ -2,14 +2,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 // Components
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, ValueComponent, NavComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
